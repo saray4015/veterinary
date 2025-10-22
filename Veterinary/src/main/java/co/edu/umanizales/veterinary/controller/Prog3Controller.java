64ca1c4c535d29767306808eaf2e6d8407306a7a
saray4015 <sperez117595@umanizales.edu.co>
2025-10-21 23:39:44 -0500

    Add files via upload

---
+++ Veterinary/src/main/java/co/edu/umanizales/veterinary/controller/Prog3Controller.java
@@ -0,0 +1,12 @@
+package co.edu.umanizales.veterinary.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org. springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/prog3")
+public class Prog3Controller {
+   @GetMapping
+    public String getHello(){
+        return "Hola campeones y campeonas";
+    }
+}
